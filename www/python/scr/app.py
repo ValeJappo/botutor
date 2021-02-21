a89a9ece56c27fd0ccbd0cbc39db7fdf10a6cacc
ValeJappo <79359669+ValeJappo@users.noreply.github.com>
2021-02-21 22:02:32 +0100

    Update app.py

--- www/python/scr/app.py
+++ www/python/scr/app.py
@@ -1,9 +1,1 @@
-import os
-
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tutor.settings")
-
-app = get_wsgi_application()
-
 print('app.py running')

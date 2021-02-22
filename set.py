85d1f04c1469a29666ad9e2eadcfd6946a7c9249
ValeJappo <79359669+ValeJappo@users.noreply.github.com>
2021-02-22 09:06:07 +0100

    Update set.py

--- set.py
+++ set.py
@@ -2,8 +2,8 @@ from configparser import ConfigParser
 config_object = ConfigParser()
 
 config_object["INFO"] = {
-    "username": str(raw_input("Username :"),
-    "password": "str(raw_input("Password :")
+    "username": str(raw_input("Username :")),
+    "password": "str(raw_input("Password :"))
 }
 
 with open('config.conf', 'w') as conf:

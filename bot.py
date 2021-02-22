c07182ad8e492f7601c9855a26643f23ed3db725
ValeJappo <79359669+ValeJappo@users.noreply.github.com>
2021-02-22 10:27:03 +0100

    Indentation

--- bot.py
+++ bot.py
@@ -13,10 +13,10 @@ URL = "https://test.wikipedia.org/w/api.php"
 
 # Token login
 PARAMS0 = {
-    'action':"query",
-    'meta':"tokens",
-    'type':"login",
-    'format':"json"
+	'action':"query",
+	'meta':"tokens",
+	'type':"login",
+	'format':"json"
 }
 
 R = S.get(url=URL, params=PARAMS0)
@@ -29,12 +29,12 @@ print(LOGIN_TOKEN)
 # Login
 
 PARAMS1 = {
-    'action': "clientlogin",
-    'username': username,
-    'password': password,
-    'loginreturnurl': 'http://127.0.0.1:5000/',
-    'logintoken': LOGIN_TOKEN,
-    'format': "json"
+	'action': "clientlogin",
+	'username': username,
+	'password': password,
+	'loginreturnurl': 'http://127.0.0.1:5000/',
+	'logintoken': LOGIN_TOKEN,
+	'format': "json"
 }
 
 R = S.post(URL, data=PARAMS1)
@@ -44,12 +44,12 @@ if DATA1["clientlogin"]["status"]=="PASS":
 else:
 	print("Login error."+DATA1)
 	raise SystemExit
-	
+
 #Token CSRF
 PARAMS2 = {
-    "action": "query",
-    "meta": "tokens",
-    "format": "json"
+	"action": "query",
+	"meta": "tokens",
+	"format": "json"
 }
 
 R = S.get(url=URL, params=PARAMS2)
@@ -80,37 +80,37 @@ except (KeyError, NameError) as error:
 	lasttimestamp=0
 	PARAMS3["rclimit"]=1
 	PARAMS3["rcdir"]="older"
-	
+
 R = S.get(url=URL, params=PARAMS3)
 DATA3 = R.json()
 
 RECENTCHANGES = DATA3['query']['recentchanges']
 
 for rc in RECENTCHANGES:
-    PARAMS4={
-        "action": "query",
-        "format": "json",
-        "list": "users",
-        "usprop": "blockinfo|editcount|gender",
-        "ususers": rc['user']
-    }
-    R = S.get(url=URL, params=PARAMS4)
-    DATA4 = R.json()
-    print(DATA4)
-    USERS=DATA4['query']['users']
-    for us in USERS:
+PARAMS4={
+	"action": "query",
+	"format": "json",
+	"list": "users",
+	"usprop": "blockinfo|editcount|gender",
+	"ususers": rc['user']
+}
+R = S.get(url=URL, params=PARAMS4)
+DATA4 = R.json()
+print(DATA4)
+USERS=DATA4['query']['users']
+for us in USERS:
 	print(us)
-	try:
-		edcount=us['editcount']
-	except KeyError: #Anonym users
-		edcount=0
-        if edcount < 100 and rc['timestamp']!=lasttimestamp:
-            print(str(us['name'])) 
-            lasttimestamp=rc['timestamp']
-            config_object["DATA"]={
-		    "timestamp": lasttimestamp
-	    }
-            with open('config.conf', 'w') as conf:
-              config_object.write(conf)
-        else:
-            print("NO - "+str(us['name'])) 
+		try:
+			edcount=us['editcount']
+		except KeyError: #Anonym users
+			edcount=0
+	if edcount < 100 and rc['timestamp']!=lasttimestamp:
+		print(str(us['name'])) 
+		lasttimestamp=rc['timestamp']
+		config_object["DATA"]={
+			"timestamp": lasttimestamp
+		}
+		with open('config.conf', 'w') as conf:
+		config_object.write(conf)
+	else:
+		print("NO - "+str(us['name'])) 

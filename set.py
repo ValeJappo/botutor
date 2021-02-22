1853472b3f65e79944bbc0c9376cfb662239a924
ValeJappo <79359669+ValeJappo@users.noreply.github.com>
2021-02-22 09:03:24 +0100

    Create set.py

---
+++ set.py
@@ -0,0 +1,12 @@
+from configparser import ConfigParser
+config_object = ConfigParser()
+
+config_object["INFO"] = {
+    "username": str(raw_input("Username :"),
+    "password": "str(raw_input("Password :")
+}
+
+with open('config.conf', 'w') as conf:
+    config_object.write(conf)
+
+

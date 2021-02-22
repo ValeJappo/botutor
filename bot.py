581ce83142b138ca8b4cceea3fcc78aa28c0c917
ValeJappo <79359669+ValeJappo@users.noreply.github.com>
2021-02-22 10:04:05 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -62,7 +62,7 @@ CSRF_TOKEN = DATA2['query']['tokens']['csrftoken']
 try:
 	PARAMS3["rcstart"]=str(config_object["DATA"]["timestamp"])
 except KeyError:
-	lasttimestamp=null
+	lasttimestamp=0
 
 PARAMS3 ={
 	"action": "query",

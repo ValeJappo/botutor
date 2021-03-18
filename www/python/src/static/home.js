e84316030e9a5af0536cd045f6783f326e2ed8a3
ValeJappo <cioffval@gmail.com>
2021-03-18 17:53:03 +0100

    polishing, simplifications and improvements

--- www/python/src/static/home.js
+++ www/python/src/static/home.js
@@ -27,7 +27,261 @@ HomePage.prototype.setupOutlineItem = function () {
 //Create Follow page
 function FollowPage( name, config ) {
     FollowPage.super.call( this, name, config );
-    this.$element.attr("id", "p-segui");
+
+    //Define variables
+    var isMsgOn=false; //Checks if there is already a warning message on the top of the page
+    var msg_el; //Stores the current warning message's element
+
+    //Toggle the warning message
+    function toggle_alert(type, msg){
+        if(!isMsgOn){
+            //Create message widget
+            msg_el=new OO.ui.MessageWidget( {
+                type: type,
+                label: msg
+            } );
+            isMsgOn=true;
+            //Create close button
+            close_btn=new OO.ui.ButtonWidget( {
+                icon: 'close',
+                label: 'Nascondi',
+                invisibleLabel: true,
+                framed: false,
+                title: 'Nascondi messaggio'
+            } );
+            close_btn.on("click", function(){msg_el.$element.remove(); isMsgOn=false;});
+            close_btn.$element.attr("style", "position:relative; top:-.35em; float:right");
+
+            //Append
+            msg_el.$icon.after(close_btn.$element);
+            booklet.getCurrentPage().$element.prepend(msg_el.$element);
+        }else{
+            //Change current message
+            msg_el.setType(type);
+            msg_el.setLabel(msg);
+        }
+    }
+
+    //Start following a user
+    function segui(user, options){
+        if (user!=""){
+            options.forEach(function(item){
+                //Fetch (see app.py for details on how follow works)
+                fetch("/action/segui/"+user+"/"+item)
+                .then(function(response){return response.json();})
+                .then(function(response) {
+                    //Return response in alert
+                    if(response.status=='success')
+                        toggle_alert("success", user+" aggiunto agli utenti osservati");
+                    else if(response.status=='duplicate')
+                        toggle_alert("warning", user+" fa già parte dei tuoi utenti osservati");
+                    else if(response.status=='login')
+                        toggle_alert("error", "Qualcosa è andato storto ed il tuo accesso con OAuth sembra essere scaduto. Prova a ricaricare la pagina.");
+                    else
+                        toggle_alert("error", "Errore nell'aggiungere "+user+" agli utenti osservati");
+                });
+            });
+        }
+    }
+
+    //Stop following a user
+    function rimuovi(user){
+        if (user!=""){
+            //Fetch (see app.py for details on how follow works)
+            fetch("/action/segui/"+user+"/rimuovi")
+                .then(function(response){return response.json();})
+                .then(function(response) {
+                    //Return response in alert
+                    if(response.status=='success')
+                        toggle_alert("success", user+" rimosso dagli utenti osservati");
+                    else
+                        toggle_alert("error", "Errore nel rimuovere "+user+" dagli utenti osservati");
+                });
+        }
+    }
+
+    //Create multi select widget
+    var multiselect=new OO.ui.MenuTagMultiselectWidget( {
+        selected: [
+            {
+                data: 'all',
+                label: '(tutte le modifiche)'
+            }
+            ],
+        options: [
+            {
+                data: 'all',
+                label: '(tutte le modifiche)'
+            },
+            {
+                data: 'talk',
+                label: 'Modifiche alle pagine di discussione'
+            },
+            {
+                data: 'ns0',
+                label: 'Modifiche al namespace principale'
+            },
+            {
+                data: 'sandbox',
+                label: 'Modifiche alla sandbox'
+            },
+            {
+                data: 'ping',
+                label: 'Modifiche nella sua pagina di discussione senza template Ping'
+            },
+        ],
+        classes: ['segui-tags']
+    } );
+    var save_loop=false; //variable to avoid entering a loop
+    //On event change
+    multiselect.on("change", function(items){
+        if(items.length>0){ //If there are items
+            if((items[items.length-1].data=="all" && items.length>1)){ //If the last one is 'all' and there are others selected
+
+                //Unselect everything apart from 'all'
+                multiselect.getMenu().items.forEach(function(i){
+                    multiselect.getMenu().unselectItem(i);
+                });
+                multiselect.setValue(["all"]);
+
+            }else if((multiselect.findItemFromData("all")!=null && items.length !=1)){ //If 'all' is selected and another item get selected
+                //Unselect 'all'
+                multiselect.removeTagByData("all");
+                save_loop=true;
+            }
+        }
+    });
+    
+    //Create a User-Lookup text input widget
+
+    /*
+    Adapted from DemoNumberLookupTextInputWidget
+    https://gerrit.wikimedia.org/g/oojs/ui/+/master/demos/classes/NumberLookupTextInputWidget.js
+    */
+
+    UserLookupTextInputWidget = function UserLookupTextInputWidget( config ) {
+        // Parent constructor
+        OO.ui.TextInputWidget.call( this, $.extend( { validate: 'string'}, config ) );
+        // Mixin constructors
+        OO.ui.mixin.LookupElement.call( this, config );
+    };
+    OO.inheritClass( UserLookupTextInputWidget, OO.ui.TextInputWidget );
+    OO.mixinClass( UserLookupTextInputWidget, OO.ui.mixin.LookupElement );
+
+    UserLookupTextInputWidget.prototype.getLookupRequest = function () {
+        var
+            value = this.getValue(),
+            deferred = $.Deferred();
+        
+        // Provide users as options
+        this.getValidity().then( function () {
+            // Resolve with API results
+            var url = "https://it.wikipedia.org/w/api.php"; 
+            var params = {
+                "action": "query",
+                "format": "json",
+                "list": "allusers",
+                "auprefix": value
+            };
+            url = url + "?origin=*";
+            Object.keys(params).forEach(function(key){url += "&" + key + "=" + params[key];});
+            fetch(url)
+                .then(function(response){return response.json();})
+                .then(function(response) {
+                    var list=[];
+                    //Add to list each user name
+                    response.query.allusers.forEach(function(user){
+                        list.push(user.name);
+                    });
+                    deferred.resolve(list);
+                })
+                .catch(function(error){console.log(error);});
+
+        }, function () {
+            // No results when the input contains invalid content
+            deferred.resolve( [] );
+        } );
+        return deferred.promise( { abort: function () {} } );
+    };
+
+        UserLookupTextInputWidget.prototype.getLookupCacheDataFromResponse = function ( response ) {
+        return response || [];
+    };
+
+        UserLookupTextInputWidget.prototype.getLookupMenuOptionsFromData = function ( data ) {
+        var
+            items = [],
+            i, number;
+        for ( i = 0; i < data.length; i++ ) {
+            number = String( data[ i ] );
+            items.push( new OO.ui.MenuOptionWidget( {
+                data: number,
+                label: number
+            } ) );
+        }
+        return items;
+    };
+
+    //Create new fieldset layout
+    var txtinput = new OO.ui.FieldsetLayout( {
+        label: null,
+        items: [
+            new OO.ui.FieldLayout( new UserLookupTextInputWidget({
+                icon: 'userAvatarOutline',
+                autocomplete: false
+            }), {
+                align: 'top',
+                label: 'Scegli un utente'
+            } ),
+            new OO.ui.FieldLayout( new OO.ui.Widget( {
+                content: [
+                    multiselect
+                ]
+            }),
+            {
+                align: 'top',
+                label: "Seleziona le modifiche per le quali vuoi ricevere una notifica"
+            }),
+            new OO.ui.FieldLayout( new OO.ui.Widget( {
+                content: [
+                    new OO.ui.HorizontalLayout( {
+                        items: [
+                            new OO.ui.ButtonInputWidget( {
+                                type: 'submit',
+                                name: 'segui',
+                                label: 'Segui',
+                                flags: [
+                                    'primary',
+                                    'progressive'
+                                ],
+                                icon: 'userAdd',
+                                classes: ['segui-add']
+                            } ),
+                            new OO.ui.ButtonWidget( {
+                                framed: false,
+                                flags: [
+                                    'destructive'
+                                ],
+                                label: 'Smetti di seguire',
+                                classes: [
+                                    'segui-remove'
+                                ]
+                            } )
+                        ]
+                    } )
+                ]
+            } ), {
+                align: 'top',
+                label: null
+            } )
+        ]
+    } );
+
+    this.$element.append(txtinput.$element);
+
+    txtinput.items[2].$element.find('.segui-add').on("click", function(){segui(txtinput.$element.find("input").val(), multiselect.getValue())});
+    txtinput.items[2].$element.find('.segui-remove').on("click", function(){rimuovi(txtinput.$element.find("input").val())});
+
 }
 OO.inheritClass( FollowPage, OO.ui.PageLayout );
 FollowPage.prototype.setupOutlineItem = function () {
